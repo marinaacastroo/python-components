7511fcff04cdf1a44457f952dde7b9a16959ca87
marinaacastroo <marina@marinaacastroo24@gmail.com>
2025-06-29 23:46:39 +0200

    cambios

--- src/main/python/programmingtheiot/cda/connection/MqttClientConnector.py
+++ src/main/python/programmingtheiot/cda/connection/MqttClientConnector.py
@@ -20,172 +20,140 @@ from programmingtheiot.common.ResourceNameEnum import ResourceNameEnum
 from programmingtheiot.cda.connection.IPubSubClient import IPubSubClient
 
 class MqttClientConnector(IPubSubClient):
-	"""
-	Shell representation of class for student implementation.
-	
-	"""
+    """
+    Shell representation of class for student implementation.
+    """
 
-	def __init__(self, clientID: str = None):
-		self.config = ConfigUtil()
-		self.dataMsgListener = None
+    def __init__(self, clientID: str = None):
+        self.config = ConfigUtil()
+        self.dataMsgListener = None
 
-		self.host = \
-			self.config.getProperty( \
-				ConfigConst.MQTT_GATEWAY_SERVICE, ConfigConst.HOST_KEY, ConfigConst.DEFAULT_HOST)
+        self.host = \
+            self.config.getProperty(
+                ConfigConst.MQTT_GATEWAY_SERVICE, ConfigConst.HOST_KEY, ConfigConst.DEFAULT_HOST)
 
-		self.port = \
-			self.config.getInteger( \
-				ConfigConst.MQTT_GATEWAY_SERVICE, ConfigConst.PORT_KEY, ConfigConst.DEFAULT_MQTT_PORT)
+        self.port = \
+            self.config.getInteger(
+                ConfigConst.MQTT_GATEWAY_SERVICE, ConfigConst.PORT_KEY, ConfigConst.DEFAULT_MQTT_PORT)
 
-		self.keepAlive = \
-			self.config.getInteger( \
-				ConfigConst.MQTT_GATEWAY_SERVICE, ConfigConst.KEEP_ALIVE_KEY, ConfigConst.DEFAULT_KEEP_ALIVE)
+        self.keepAlive = \
+            self.config.getInteger(
+                ConfigConst.MQTT_GATEWAY_SERVICE, ConfigConst.KEEP_ALIVE_KEY, ConfigConst.DEFAULT_KEEP_ALIVE)
 
-		self.defaultQos = \
-			self.config.getInteger( \
-				ConfigConst.MQTT_GATEWAY_SERVICE, ConfigConst.DEFAULT_QOS_KEY, ConfigConst.DEFAULT_QOS)
+        self.defaultQos = \
+            self.config.getInteger(
+                ConfigConst.MQTT_GATEWAY_SERVICE, ConfigConst.DEFAULT_QOS_KEY, ConfigConst.DEFAULT_QOS)
 
-		self.mqttClient = None
+        self.mqttClient = None
 
-		# IMPORTANT:
-		#
-		# You can choose to set clientID in a number of ways:
-		#  1 - use the locationID value in PiotConfig.props as the clientID (see below)
-		#  2 - pass a custom clientID into constructor (from DeviceDataManager or your test)
-		#  3 - hard code a clientID in this constructor (generally not recommended)
-		#  4 - if using Python Paho, set NO client ID and let broker auto-assign
-		#      a random value (not recommended if setting clean session flag to False)
+        if not clientID:
+            self.clientID = \
+                self.config.getProperty(
+                    ConfigConst.CONSTRAINED_DEVICE, ConfigConst.DEVICE_LOCATION_ID_KEY)
+        else:
+            self.clientID = clientID
 
-		# TODO: the following is just a sample; use your own unique ID
-		if not clientID:
-			self.clientID = \
-				self.config.getProperty( \
-					ConfigConst.CONSTRAINED_DEVICE, ConfigConst.DEVICE_LOCATION_ID_KEY)
+        logging.info('\tMQTT Client ID:   ' + self.clientID)
+        logging.info('\tMQTT Broker Host: ' + self.host)
+        logging.info('\tMQTT Broker Port: ' + str(self.port))
+        logging.info('\tMQTT Keep Alive:  ' + str(self.keepAlive))
 
-		else: 
-			self.clientID = clientID
+    def connectClient(self) -> bool:
+        if not self.mqttClient:
+            self.mqttClient = mqttClient.Client(client_id=self.clientID, clean_session=True)
 
-		logging.info('\tMQTT Client ID:   ' + self.clientID)
-		logging.info('\tMQTT Broker Host: ' + self.host)
-		logging.info('\tMQTT Broker Port: ' + str(self.port))
-		logging.info('\tMQTT Keep Alive:  ' + str(self.keepAlive))
+            self.mqttClient.on_connect = self.onConnect
+            self.mqttClient.on_disconnect = self.onDisconnect
+            self.mqttClient.on_message = self.onMessage
+            self.mqttClient.on_publish = self.onPublish
+            self.mqttClient.on_subscribe = self.onSubscribe
 
-	def connectClient(self) -> bool:
-		if not self.mqttClient:
-			# TODO: make clean_session configurable
-			self.mqttClient = mqttClient.Client(client_id = self.clientID, clean_session = True)
+        if not self.mqttClient.is_connected():
+            logging.info('MQTT client connecting to broker at host: ' + self.host)
+            self.mqttClient.connect(self.host, self.port, self.keepAlive)
+            self.mqttClient.loop_start()
 
-			self.mqttClient.on_connect = self.onConnect
-			self.mqttClient.on_disconnect = self.onDisconnect
-			self.mqttClient.on_message = self.onMessage
-			self.mqttClient.on_publish = self.onPublish
-			self.mqttClient.on_subscribe = self.onSubscribe
+            return True
+        else:
+            logging.warning('MQTT client is already connected. Ignoring connect request.')
 
-		if not self.mqttClient.is_connected():
-			logging.info('MQTT client connecting to broker at host: ' + self.host)
-			self.mqttClient.connect(self.host, self.port, self.keepAlive)
-			self.mqttClient.loop_start()
+            return False
 
-			return True
-		else:
-			logging.warning('MQTT client is already connected. Ignoring connect request.')
+    def disconnectClient(self) -> bool:
+        if self.mqttClient and self.mqttClient.is_connected():
+            logging.info('Disconnecting MQTT client from broker: ' + self.host)
+            self.mqttClient.loop_stop()
+            self.mqttClient.disconnect()
 
-			return False
-		
-	def disconnectClient(self) -> bool:
-		if self.mqttClient.is_connected():
-			logging.info('Disconnecting MQTT client from broker: ' + self.host)
-			self.mqttClient.loop_stop()
-			self.mqttClient.disconnect()
+            return True
+        else:
+            logging.warning('MQTT client already disconnected. Ignoring.')
 
-			return True
-		else:
-			logging.warning('MQTT client already disconnected. Ignoring.')
+            return True  # <-- Cambia esto a True
 
-			return False
-		
-	def onConnect(self, client, userdata, flags, rc):
-		logging.info('MQTT client connected to broker: ' + str(client))
+    def onConnect(self, client, userdata, flags, rc):
+        logging.info('MQTT client connected to broker: ' + str(client))
 
-	def onDisconnect(self, client, userdata, rc):
-		logging.info('MQTT client disconnected from broker: ' + str(client))
+    def onDisconnect(self, client, userdata, rc):
+        logging.info('MQTT client disconnected from broker: ' + str(client))
 
-	def onMessage(self, client, userdata, msg):
-		payload = msg.payload
-		if payload:
-			logging.info('MQTT message received with payload: ' + str(payload.decode("utf-8")))
-		else:
-			logging.info('MQTT message received with no payload: ' + str(msg))
+    def onMessage(self, client, userdata, msg):
+        payload = msg.payload
+        if payload:
+            logging.info('MQTT message received with payload: ' + str(payload.decode("utf-8")))
+        else:
+            logging.info('MQTT message received with no payload: ' + str(msg))
 
-	def onPublish(self, client, userdata, mid):
-		pass
+    def onPublish(self, client, userdata, mid):
+        # logging.info('Message published successfully')  # Disabled for performance test
+        pass
 
-	def onSubscribe(self, client, userdata, mid, granted_qos):
-		logging.info('MQTT client subscribed: ' + str(client))
-	
-	def onActuatorCommandMessage(self, client, userdata, msg):
-		"""
-		This callback is defined as a convenience, but does not
-		need to be used and can be ignored.
-		
-		It's simply an example for how you can create your own
-		custom callback for incoming messages from a specific
-		topic subscription (such as for actuator commands).
-		
-		@param client The client reference context.
-		@param userdata The user reference context.
-		@param msg The message context, including the embedded payload.
-		"""
-		pass
-	
-	def publishMessage(self, resource: ResourceNameEnum = None, msg: str = None, qos: int = ConfigConst.DEFAULT_QOS) -> bool:
-		# check validity of resource (topic)
-		if not resource:
-			# logging.warning('No topic specified. Cannot publish message.')  # Disabled for performance test
-			return False
+    def onSubscribe(self, client, userdata, mid, granted_qos):
+        logging.info('MQTT client subscribed: ' + str(client))
 
-		# check validity of message
-		if not msg:
-			# logging.warning('No message specified. Cannot publish message to topic: ' + resource.value)  # Disabled for performance test
-			return False
+    def onActuatorCommandMessage(self, client, userdata, msg):
+        pass
 
-		# check validity of QoS - set to default if necessary
-		if qos < 0 or qos > 2:
-			qos = ConfigConst.DEFAULT_QOS
+    def publishMessage(self, resource: ResourceNameEnum = None, msg: str = None, qos: int = ConfigConst.DEFAULT_QOS) -> bool:
+        # if not resource:
+        #     logging.warning('No topic specified. Cannot publish message.')  # Disabled for performance test
+        #     return False
 
-		# publish message, and wait for publish to complete before returning
-		msgInfo = self.mqttClient.publish(topic=resource.value, payload=msg, qos=qos)
-		msgInfo.wait_for_publish()
+        # if not msg:
+        #     logging.warning('No message specified. Cannot publish message to topic: ' + resource.value)  # Disabled for performance test
+        #     return False
 
-		return True
+        if qos < 0 or qos > 2:
+            qos = ConfigConst.DEFAULT_QOS
 
-	def subscribeToTopic(self, resource: ResourceNameEnum = None, callback=None, qos: int = ConfigConst.DEFAULT_QOS) -> bool:
-		# check validity of resource (topic)
-		if not resource:
-			logging.warning('No topic specified. Cannot subscribe.')
-			return False
+        msgInfo = self.mqttClient.publish(topic=resource.value, payload=msg, qos=qos)
+        msgInfo.wait_for_publish()  # Wait for publish to complete for performance test
 
-		# check validity of QoS - set to default if necessary
-		if qos < 0 or qos > 2:
-			qos = ConfigConst.DEFAULT_QOS
+        return True
 
-		# subscribe to topic
-		logging.info('Subscribing to topic %s', resource.value)
-		self.mqttClient.subscribe(resource.value, qos)
+    def subscribeToTopic(self, resource: ResourceNameEnum = None, callback=None, qos: int = ConfigConst.DEFAULT_QOS) -> bool:
+        if not resource:
+            logging.warning('No topic specified. Cannot subscribe.')
+            return False
 
-		return True
+        if qos < 0 or qos > 2:
+            qos = ConfigConst.DEFAULT_QOS
 
-	def unsubscribeFromTopic(self, resource: ResourceNameEnum = None) -> bool:
-		# check validity of resource (topic)
-		if not resource:
-			logging.warning('No topic specified. Cannot unsubscribe.')
-			return False
+        logging.info('Subscribing to topic %s', resource.value)
+        self.mqttClient.subscribe(resource.value, qos)
 
-		logging.info('Unsubscribing to topic %s', resource.value)
-		self.mqttClient.unsubscribe(resource.value)
+        return True
 
-		return True
+    def unsubscribeFromTopic(self, resource: ResourceNameEnum = None) -> bool:
+        if not resource:
+            logging.warning('No topic specified. Cannot unsubscribe.')
+            return False
 
-	def setDataMessageListener(self, listener: IDataMessageListener = None):
-		if listener:
-			self.dataMsgListener = listener
+        logging.info('Unsubscribing to topic %s', resource.value)
+        self.mqttClient.unsubscribe(resource.value)
+
+        return True
+
+    def setDataMessageListener(self, listener: IDataMessageListener = None):
+        if listener:
+            self.dataMsgListener = listener
--- src/test/python/programmingtheiot/part03/integration/connection/MqttClientPerformanceTest.py
+++ src/test/python/programmingtheiot/part03/integration/connection/MqttClientPerformanceTest.py
@@ -7,81 +7,57 @@
 # Copyright (c) 2020 by Andrew D. King
 # 
 
-import logging
 import unittest
-
 import time
+import logging
 
 from programmingtheiot.cda.connection.MqttClientConnector import MqttClientConnector
-
 from programmingtheiot.common.ResourceNameEnum import ResourceNameEnum
-
-from programmingtheiot.data.DataUtil import DataUtil
 from programmingtheiot.data.SensorData import SensorData
+from programmingtheiot.data.DataUtil import DataUtil
 
-class MqttClientConnectorTest(unittest.TestCase):
-	"""
-	This test case class contains very basic unit tests for
-	MqttClientConnector. It should not be considered complete,
-	but serve as a starting point for the student implementing
-	additional functionality within their Programming the IoT
-	environment.
-	"""
-	NS_IN_MILLIS = 1000000
-	MAX_TEST_RUNS = 10000
-	
-	@classmethod
-	def setUpClass(self):
-		logging.basicConfig(format = '%(asctime)s:%(module)s:%(levelname)s:%(message)s', level = logging.DEBUG)
-		
-	def setUp(self):
-		self.mqttClient = MqttClientConnector(clientID = 'CDAMqttClientPerformanceTest001')
-		pass
+class MqttClientPerformanceTest(unittest.TestCase):
+    NS_IN_MILLIS = 1000000
+    MAX_TEST_RUNS = 10000
 
-	def tearDown(self):
-		pass
+    @classmethod
+    def setUpClass(cls):
+        logging.basicConfig(format = '%(asctime)s:%(module)s:%(levelname)s:%(message)s', level = logging.INFO)
 
-	def testConnectAndDisconnect(self):
-		startTime = time.time_ns()
-		
-		self.assertTrue(self.mqttClient.connectClient())
-		self.assertTrue(self.mqttClient.disconnectClient())
-		
-		endTime = time.time_ns()
-		elapsedMillis = (endTime - startTime) / self.NS_IN_MILLIS
-		
-		logging.info("Connect and Disconnect: " + str(elapsedMillis) + " ms")
-		
-	def testPublishQoS0(self):
-		self._execTestPublish(self.MAX_TEST_RUNS, 0)
+    def setUp(self):
+        self.mqttClient = MqttClientConnector(clientID = 'CDAMqttClientPerformanceTest001')
 
-	def testPublishQoS1(self):
-		self._execTestPublish(self.MAX_TEST_RUNS, 1)
+    def tearDown(self):
+        pass
 
-	def testPublishQoS2(self):
-		self._execTestPublish(self.MAX_TEST_RUNS, 2)
+    def testConnectAndDisconnect(self):
+        startTime = time.time_ns()
+        self.assertTrue(self.mqttClient.connectClient())
+        self.assertTrue(self.mqttClient.disconnectClient())
+        endTime = time.time_ns()
+        elapsedMillis = (endTime - startTime) / self.NS_IN_MILLIS
+        logging.info("Connect and Disconnect: " + str(elapsedMillis) + " ms")
 
-	def _execTestPublish(self, maxTestRuns: int, qos: int):
-		self.assertTrue(self.mqttClient.connectClient())
-		
-		sensorData = SensorData()
-		payload = DataUtil().sensorDataToJson(sensorData)
-		payloadLen = len(payload)
-		startTime = time.time_ns()
-		
-		for seqNo in range(0, maxTestRuns):
-			self.mqttClient.publishMessage(resource = ResourceNameEnum.CDA_SENSOR_MSG_RESOURCE, msg = payload, qos = qos)
-			
-		endTime = time.time_ns()
-		elapsedMillis = (endTime - startTime) / self.NS_IN_MILLIS
-		
-		self.assertTrue(self.mqttClient.disconnectClient())
-		
-		logging.info( \
-			"\n\tTesting Publish: QoS = %r | msgs = %r | payload size = %r | start = %r | end = %r | elapsed = %r", \
-			qos, maxTestRuns, payloadLen, startTime / 1000, endTime / 1000, elapsedMillis / 1000)
-		
-		#logging.info("Publish message - QoS " + str(qos) + " [" + str(maxTestRuns) + "]: " + str(elapsedMillis) + " ms")
-	
+    def testPublishQoS0(self):
+        self._execTestPublish(self.MAX_TEST_RUNS, 0)
+
+    def testPublishQoS1(self):
+        self._execTestPublish(self.MAX_TEST_RUNS, 1)
+
+    def testPublishQoS2(self):
+        self._execTestPublish(self.MAX_TEST_RUNS, 2)
+
+    def _execTestPublish(self, maxTestRuns: int, qos: int):
+        self.assertTrue(self.mqttClient.connectClient())
+        sensorData = SensorData()
+        payload = DataUtil().sensorDataToJson(sensorData)
+        startTime = time.time_ns()
+        for seqNo in range(0, maxTestRuns):
+            self.mqttClient.publishMessage(resource = ResourceNameEnum.CDA_SENSOR_MSG_RESOURCE, msg = payload, qos = qos)
+        endTime = time.time_ns()
+        elapsedMillis = (endTime - startTime) / self.NS_IN_MILLIS
+        self.assertTrue(self.mqttClient.disconnectClient())
+        logging.info("Publish message - QoS " + str(qos) + " [" + str(maxTestRuns) + "]: " + str(elapsedMillis) + " ms")
+
 if __name__ == "__main__":
-	unittest.main()
+    unittest.main()
